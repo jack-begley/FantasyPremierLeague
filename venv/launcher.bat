216067a4025ed42c0f8c2b1c2a84e5aca756ab6a
Jack Begley <jack.begley@morarhpi.com>
2023-07-23 19:25:08 +0100

    Update the venv environment

---
+++ venv/launcher.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Activate the Flask venv
+venv\Scripts\activate
+REM Run Flask server
+python app.py
+REM Deactivate the Flask venv
+deactivate
+REM Run npm commands
+cd C:\Users\JackBegley\source\repos\FantasyPremierLeague\client\src
+npm start
