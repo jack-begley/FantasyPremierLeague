71d2583235d9564b584ad074ab4d9685a5f9005f
Jack Begley <jack.begley@morarhpi.com>
2023-07-31 11:43:27 +0100

    Initial set up trying player surname matching

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Players = () => {
+    const [playerData, setPlayerData] = useState(null);
+
+    useEffect(() => {
+        // replace 'surname' with the player's surname you're interested in
+        const playerSurname = 'salah';
+        axios.get(`http://localhost:5000/playerinfo?surname=${playerSurname}`)
+            .then(response => {
+                setPlayerData(response.data);
+            })
+            .catch(error => console.error(`Error: ${error}`));
+    }, []);
+
     return (
         <div>
-            <h1>Welcome to the Home Page</h1>
-            <p>This is where the player content will be</p>
+            {playerData && (
+                <div>
+                    {/* Display player data here */}
+                    <p>{`Player name: ${playerData.first_name} ${playerData.second_name}`}</p>
+                    <p>{`Form: ${playerData.form}`}</p>
+                    {/* Add more data as needed */}
+                </div>
+            )}
         </div>
     );
 };
